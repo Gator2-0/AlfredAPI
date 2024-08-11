cd5751317507c64f432408beda567ff95aee73df
Gator2-0 <jejedech.63@gmail.com>
2024-08-11 23:06:00 +1000

    initial commit

---
+++ Controllers/DivingControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AfredAPI.Controllers
+{
+    public class DivingControllers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
